c784d8868e2e66976031ad7c212928fb77ff7712
gprzy <gabrielhprzytocki@gmail.com>
2023-03-07 01:01:06 -0300

    feat: adding submodule paths

---
+++ add_submodules.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+git submodule add https://github.com/gprzy/modeling-of-physical-phenomena.git modeling-of-physical-phenomena
+git submodule add https://github.com/gprzy/metropolitan-region-graph.git metropolitan-region-graph
+git submodule add https://github.com/gprzy/enron-graph-analysis.git enron-graph-analysis
+git submodule add https://github.com/gprzy/transactions-pyspark.git transactions-pyspark
+git submodule add https://github.com/gprzy/credit-scoring.git credit-scoring
+git submodule add https://github.com/gprzy/linear-programming.git linear-programming
+git submodule add https://github.com/gprzy/ahp.git ahp
+git submodule add https://github.com/gprzy/master-theorem-analysis.git master-theorem-analysis
+git submodule add https://github.com/gprzy/sorting-complexity-analysis.git sorting-complexity-analysis
+git submodule add https://github.com/gprzy/casino-monte-carlo.git casino-monte-carlo
+git submodule add https://github.com/gprzy/euromillions-jackpot.git euromillions-jackpot
+git submodule add https://github.com/gprzy/pyro5-voter.git pyro5-voter
+git submodule add https://github.com/gprzy/vehicles-counting.git vehicles-counting
+git submodule add https://github.com/gprzy/object-tracking.git object-tracking
+git submodule add https://github.com/gprzy/license-plate-recognition.git license-plate-recognition
+git submodule add https://github.com/gprzy/simpsons-classification.git simpsons-classification
